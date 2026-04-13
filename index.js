d5775b34e594a2883dba7a4278f0760877c2acb3
Aman Tomar <amantoma700@gmail.com>
2026-04-13 14:29:16 +0530

    Initial commit: Setup TypeScript project

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sum(a, b) {
+    console.log(a + b);
+}
+sum(5, 3);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,4 @@
+function sum (a : number,b:number) {
+    console.log(a+b);
+}
+sum(5, 3);touch .gitignore
